8bb6839ea1b222ab1be0c8e559e2bd1b972c8daf
Damiennsoh <psycaro20@gmail.com>
2023-03-25 02:32:17 +0530

    pseudocode for the program is outlined

---
+++ task_3.py
@@ -0,0 +1,11 @@
+# Below are my pseudocode steps as I will be following these steps to write my program in order to dispens the awards according to the award table and it's corresponding time of the three events.
+
+# Design a program that determines the award a person competing in a triathlon will receive.
+# creating an event input for all three events- thus swimming, cycling and running
+# It also means I will have to add the total time taken by each of the three events.
+# I then need to compare the combined total time of each of the three events with the award table provided.
+# The time in question is in minutes.
+# After comparing the total time taken by the three events with the award table, I can then print out my awards statements based on 
+# the combined time of the three events and what award it corresponds to.
+# I will be using my if statements to control the flow of the program as well as my decisions.
+#
