d579191242e94293f20be8c91ef3ca314cb52996
Damiennsoh <psycaro20@gmail.com>
2023-03-25 02:43:48 +0530

    created the first event time input in minutes which is running

--- task_3.py
+++ task_3.py
@@ -1,7 +1,9 @@
+# Design a program that determines the award a person competing in a triathlon will receive.
 # Below are my pseudocode steps as I will be following these steps to write my program in order to dispens the awards according to the award table and it's corresponding time of the three events.
 
-# Design a program that determines the award a person competing in a triathlon will receive.
 # creating an event input for all three events- thus swimming, cycling and running
+running = int(input("what is the time taken for swimming in minutes ? Enter it here: ")) # Here, the input function input() is by default a string, but because I'm dealing with time which is an int, I first of all need to cast the data type from string into an int. That is why the int data type is placed infront of the input function.
+print("Time taken for running is: ", running)
 # It also means I will have to add the total time taken by each of the three events.
 # I then need to compare the combined total time of each of the three events with the award table provided.
 # The time in question is in minutes.
